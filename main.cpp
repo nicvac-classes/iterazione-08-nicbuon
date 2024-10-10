8844a6442607ee3c404339d8e706cd97ddf3edd7
nicbuon <nicola.buonavoglia@marconibari.edu.it>
2024-10-10 08:06:51 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,41 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int i, n, contDebito, contPromossi;
+  double voto, somma, media;
+
+  do 
+  {
+    cout << "Quanti studenti ci sono in classe?" << endl;
+    cin >> n;
+  }
+  while (n < 0);
+  contDebito = 0;
+  contPromossi = 0;
+  somma = 0;
+  i = 0;
+  while (i < n) 
+  {
+    cout << "Inserisci il voto del " << i + 1 << " alunno" << endl;
+    cin >> voto;
+    if (voto >= 6) 
+    {
+      contPromossi = contPromossi + 1;
+    } 
+    else 
+    {
+      contDebito = contDebito + 1;
+    }
+    somma = somma + voto;
+    i = i + 1;
+  }
+  media = somma / n;
+  cout << "Saranno promossi " << contPromossi << " studenti. Avranno il debito "
+  << contDebito << " studenti" << endl;
+  cout << "La media dei voti della classe è " << media << endl;
+}
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
